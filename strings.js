52ce2f95e6e058650d07211e52d3de5a8dfb3195
Zam <jackchekhit2022working@gmail.com>
2022-11-28 05:39:29 +0300

    a

--- strings.js
+++ strings.js
@@ -3,3 +3,10 @@ var str1 = "Java";
 var str2 = "Script";
 var str3 = "15";
 var str4 = "     JavaScript is nice.          ";
+
+
+
+// Перевернуть текст по аналогии полиндрома
+const text = 'Привет, мы изучаем JavaScript'
+const reverseText = text.split('').reverse().join('')
+console.log(reverseText)
